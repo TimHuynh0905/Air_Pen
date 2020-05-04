c914f5dd7b484d47bafba233409b2038346a4feb
TimHuynh0905 <timhuynh0905@gmail.com>
2020-05-04 06:16:40 -0700

    shortened emnist path

--- source_code/check_NN.py
+++ source_code/check_NN.py
@@ -6,10 +6,10 @@ import matplotlib.pyplot as plt
 from collections import deque
 from NN_Model import NN_Model
 
-raw_X_train = idx2numpy.convert_from_file('/Users/timhuynh0905/Documents/Air_Pen/EMNIST_data/emnist-byclass-train-images-idx3-ubyte')
-raw_y_train = idx2numpy.convert_from_file('/Users/timhuynh0905/Documents/Air_Pen/EMNIST_data/emnist-byclass-train-labels-idx1-ubyte')
-raw_X_test = idx2numpy.convert_from_file('/Users/timhuynh0905/Documents/Air_Pen/EMNIST_data/emnist-byclass-test-images-idx3-ubyte')
-raw_y_test = idx2numpy.convert_from_file('/Users/timhuynh0905/Documents/Air_Pen/EMNIST_data/emnist-byclass-test-labels-idx1-ubyte')
+raw_X_train = idx2numpy.convert_from_file('EMNIST_data/emnist-byclass-train-images-idx3-ubyte')
+raw_y_train = idx2numpy.convert_from_file('EMNIST_data/emnist-byclass-train-labels-idx1-ubyte')
+raw_X_test = idx2numpy.convert_from_file('EMNIST_data/emnist-byclass-test-images-idx3-ubyte')
+raw_y_test = idx2numpy.convert_from_file('EMNIST_data/emnist-byclass-test-labels-idx1-ubyte')
 
 model= NN_Model(raw_X_train = raw_X_train,
                 raw_y_train = raw_y_train)
